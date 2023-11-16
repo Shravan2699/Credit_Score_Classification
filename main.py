d0e9ed44e0860c30add00c7f8db52fabc7e28cec
Shravan2699 <kinishravan4@gmail.com>
2023-11-16 18:32:53 -0400

    -Added documentation word file explaining what the code does

--- main.py
+++ main.py
@@ -18,18 +18,20 @@ from sklearn.metrics import accuracy_score
 from sklearn import tree
 from sklearn.metrics import mean_squared_error,mean_absolute_error,explained_variance_score
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 # from statsmodels.stats.outliers_influence import variance_inflation_factor
 import statsmodels.api as sm
 
 
 sns.set_style("whitegrid")
 sns.set_context("paper",font_scale=1.0)
+#This is just for styling
 
 # %matplotlib inline
 
-pd.options.display.max_rows = 1000
-pd.options.display.max_columns = 100
-pd.options.display.float_format = '{:.2f}'.format
+pd.options.display.max_rows = 1000 #Truncates the rows if they are over 100
+pd.options.display.max_columns = 100 #Truncates the columns if they are over 100
+pd.options.display.float_format = '{:.2f}'.format #Only displays upto 2 decimal points for numericals
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -39,16 +41,15 @@ warnings.filterwarnings('ignore')
 
 #Loading the dataset
 df = pd.read_csv('./data/train.csv')
-print(df.head())
+# print(df.head())
 
 df.columns = [x.lower() for x in df.columns]
 
-print(df.columns)
+# print(df.columns)
 
 #Checking data quality
 
-df.shape
-
+print(df.shape)
 
 #Dropping not needed columns
 df.drop(['id','customer_id','month','name','ssn','type_of_loan','credit_history_age'],axis=1,inplace=True)
@@ -56,13 +57,14 @@ df.drop(['id','customer_id','month','name','ssn','type_of_loan','credit_history_
 print(df.info())
 print(df.duplicated().value_counts()) 
 
-#Checking for null values in the dataset next
+
+# #Checking for null values in the dataset next
 print(df.isnull().sum().reset_index().rename(columns={'index': 'feature_name',0: 'null_counts'}))
 print(df.shape[0])
 
 
-#Dropping null values
-#This line filters the DataFrame df by keeping only those rows where the sum of NaN values across columns is less than 3. It drops rows that have more than 2 NaN values.
+# #Dropping null values
+# #This line filters the DataFrame df by keeping only those rows where the sum of NaN values across columns is less than 3. It drops rows that have more than 2 NaN values.
 size_before_cleaning = df.shape
 df = df[df.isnull().sum(axis=1) < 3]
 print(f"{size_before_cleaning[0] - df.shape[0]} records dropped")
@@ -100,9 +102,8 @@ df["num_of_delayed_payment"] = df["num_of_delayed_payment"].apply(filter_delayed
 df["num_of_delayed_payment"].replace('', np.nan, inplace=True)
 df["num_of_delayed_payment"] = df["num_of_delayed_payment"].astype("float")
 
-# df["num_of_delayed_payment"] = df["num_of_delayed_payment"].fillna()
 df["num_of_delayed_payment"].fillna(df["num_of_delayed_payment"].median(), inplace=True)
-# print(df["num_of_delayed_payment"].head())
+
 
 
 def filter_general(value):
@@ -114,14 +115,21 @@ def filter_general(value):
         return str(value)
     
 df.drop(df[df["monthly_balance"]=="__-333333333333333333333333333__"].index,inplace=True)
+
 for i in ['age','annual_income','num_of_loan','outstanding_debt','monthly_balance']:
     df[i] = df[i].apply(filter_general)
     df[i] = df[i].astype(np.float64)
     print(i + " Successfully Cleaned")
 
+df = df[df['age'] <= 120]
+df = df[df['num_bank_accounts'] <= 20]
+df = df[df['num_credit_card'] <= 20]
+df = df[df['num_of_loan'] <= 20]
+
 
 df['occupation'] = df['occupation'].replace('_______',np.nan)
 df["occupation"] = df["occupation"].fillna(np.random.choice(pd.Series(['Scientist','Teacher','Engineer','Entrepreneur','Developer','Lawyer','Media_Manager','Doctor','Journalist','Manager','Accountant','Musician','Mechanic','Writer','Architect'])))
+
 
 df['credit_mix'] = df['credit_mix'].replace('_',np.nan)
 df['credit_mix'] = df['credit_mix'].fillna(np.random.choice(pd.Series(['Standard','Good','Bad'])))
@@ -149,17 +157,19 @@ for i in ['monthly_inhand_salary',"amount_invested_monthly",'num_credit_inquirie
 
 
 
-#######################################################################################
+# #######################################################################################
 #EDA
 print(df.info())
 
 df_cat = df.select_dtypes(include='object')
 df_num = df.select_dtypes(include='number')
 print(df_num.info())
+print(df_cat.info())
 
-# # #Categorical features:
 
-# # #The below code gives the count of unique values in each column of the categorical columns that we have
+#Categorical features:
+
+#The below code gives the count of unique values in each column of the categorical columns that we have
 for col in df_cat:
     print(df_cat[col].value_counts())
     print('\n---------------------------')
@@ -167,19 +177,19 @@ for col in df_cat:
 
 
 # #Occupation
-# plt.figure(figsize=(10,5))
-# ax = sns.countplot(x='occupation',data=df_cat,order=df['occupation'].value_counts().index)
-# plt.title('Counts of Unique Occupations')
-# plt.xlabel('Occupation')
-# plt.ylabel('Counts')
-# for p in ax.patches:
-#     ax.annotate('{}'.format(p.get_height()),(p.get_x()+0.1,p.get_height()+50))
+plt.figure(figsize=(10,5))
+ax = sns.countplot(x='occupation',data=df_cat,order=df['occupation'].value_counts().index)
+plt.title('Counts of Unique Occupations')
+plt.xlabel('Occupation')
+plt.ylabel('Counts')
+for p in ax.patches:
+    ax.annotate('{}'.format(p.get_height()),(p.get_x()+0.1,p.get_height()+50))
 
-# plt.xticks(rotation=90)
-# plt.show()
+plt.xticks(rotation=90)
+plt.show()
 
 
-# #Credit Mix
+# # #Credit Mix
 # plt.figure(figsize=(10,5))
 # ax = sns.countplot(x='credit_mix',data=df_cat,order=df['credit_mix'].value_counts().index)
 # plt.title('Credit Mix Counts')
@@ -194,7 +204,7 @@ for col in df_cat:
 # print(df_cat.info())
 
 
-# #Payment of Minimum Amount
+# # #Payment of Minimum Amount
 # plt.figure(figsize=(10,5))
 # ax = sns.countplot(x='payment_of_min_amount',data=df_cat,order=df['payment_of_min_amount'].value_counts().index)
 # plt.title('Payment of min_amount Counts')
@@ -206,7 +216,7 @@ for col in df_cat:
 # # plt.xticks(rotation=90)
 # plt.show()
 
-# #Payment Behaviour
+# # #Payment Behaviour
 # plt.figure(figsize=(10,5))
 # ax = sns.countplot(x='payment_behaviour',data=df_cat,order=df['payment_behaviour'].value_counts().index)
 # plt.title('Payment behaviour Counts')
@@ -217,7 +227,7 @@ for col in df_cat:
 # plt.xticks(rotation=45)
 # plt.show()
 
-# #Credit Score
+# # #Credit Score
 # plt.figure(figsize=(10,5))
 # ax = sns.countplot(x='credit_score',data=df_cat,order=df['credit_score'].value_counts().index)
 # plt.title('Credit_score Counts')
@@ -229,41 +239,97 @@ for col in df_cat:
 # plt.show()
 
 
-#NUMERICAL DATA COLUMNS
+# #NUMERICAL DATA COLUMNS
 
-print(df_num.describe())
-# df_num.hist(bins=50,figsize=[20,10])
-# sns.pairplot(df_num)
-# plt.show()
 
 #Correlation Heatmap
 corr = df_num.corr()
-print(corr)
+
 
 plt.figure(figsize=(10,5))
 sns.heatmap(corr.corr(),cmap="GnBu",center=0,annot=True)
-# plt.show()
+plt.show()
 
 numeric_cols =  df.select_dtypes(exclude="object").columns
-cat_cols = df.select_dtypes(include="object").columns
-print(numeric_cols)
-print(cat_cols)
+# cat_cols = df.select_dtypes(include="object").columns
+# print(numeric_cols)
+# print(cat_cols)
 
-#VIF
+# #Credit score correlation with all the other features
+
+# #VIF
+#We will only consider the numeric columns to compute this
 vif_df = df[numeric_cols]
 X = sm.add_constant(vif_df)
 vif_data = pd.DataFrame()
 vif_data["feature"] = X.columns
+
 # Calculate VIF
-#sm.OLS(X[col], X.drop(col, axis=1)): sm refers to the statsmodels library, and OLS stands for Ordinary Least Squares, which is a method for estimating the parameters of a linear regression model.
-#For each iteration, this part of the code fits a simple linear regression model where the current column (col) is the dependent variable, and all other columns (excluding the current one) are independent variables.
-#fit(): This method fits the linear regression model to the data.
-#.rsquared: The rsquared attribute retrieves the coefficient of determination for the fitted regression model.It measures the proportion of the variance in the dependent variable explained by the independent variables.
-#The entire formula for VIF is 1/1-R^2
-#VIF scores for each variables indicate the extent to which the variance of each variable is inflated due to potential multicollinearity with other independent variables. Higher VIF values suggest a higher degree of multicollinearity for the corresponding variable.
+# sm.OLS(X[col], X.drop(col, axis=1)): sm refers to the statsmodels library, and OLS stands for Ordinary Least Squares, which is a method for estimating the parameters of a linear regression model.
+# For each iteration, this part of the code fits a simple linear regression model where the current column (col) is the dependent variable, and all other columns (excluding the current one) are independent variables.
+# fit(): This method fits the linear regression model to the data.
+# .rsquared: The rsquared attribute retrieves the coefficient of determination for the fitted regression model.It measures the proportion of the variance in the dependent variable explained by the independent variables.
+# The entire formula for VIF is 1/1-R^2
+# VIF scores for each variables indicate Athe extent to which the variance of each variable is inflated due to potential multicollinearity with other independent variables. Higher VIF values suggest a higher degree of multicollinearity for the corresponding variable.
 vif_values = [1 / (1 - sm.OLS(X[col], X.drop(col, axis=1)).fit().rsquared) for col in X.columns[1:]]
 
 # Create DataFrame
 vif_data = pd.DataFrame({'feature': X.columns[1:], 'VIF': vif_values})
 
 print(vif_data)
+
+#We will now remove the features with vif greater than 2 as values above 2 are seen as a signal that there may be multicollinearity issues.
+df = df.drop(['monthly_inhand_salary','amount_invested_monthly','monthly_balance'],axis=1)
+# df = df.dropna(subset=df.columns.values)
+
+
+#Data Preparation
+X = df.drop('credit_score', axis=1)  # Features
+y = df['credit_score']  # Target variable
+
+# # Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#We will take 20% data as test data
+
+numeric_cols_train = X_train.select_dtypes(include=['number']).columns.tolist()
+numeric_cols = [col for col in numeric_cols if col in numeric_cols_train]
+
+X_train_encoded = pd.get_dummies(X_train, columns=['occupation', 'credit_mix', 'payment_of_min_amount', 'payment_behaviour'])
+X_test_encoded = pd.get_dummies(X_test, columns=['occupation', 'credit_mix', 'payment_of_min_amount', 'payment_behaviour'])
+# # #We use one-hot encoding for our categorical features to make them binary 
+
+
+scaler = StandardScaler()
+X_train_encoded[numeric_cols] = scaler.fit_transform(X_train_encoded[numeric_cols])
+X_test_encoded[numeric_cols] = scaler.transform(X_test_encoded[numeric_cols])
+
+
+
+# # #Model-Logistic Regression
+logreg = LogisticRegression()
+logreg.fit(X_train_encoded, y_train)
+
+y_pred = logreg.predict(X_test_encoded)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+# print(df_cat.info())
+print(numeric_cols)
+print(X_train.columns)
+
+
+# #Age-outlier values-needds to be 
+
+# print(df.describe())
+
+# print(X_train_encoded.columns)
+# print(X_test_encoded.columns)
+
+# cm = confusion_matrix(y_test, y_pred)
+# plt.figure(figsize=(8, 6))
+# sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=logreg.classes_, yticklabels=logreg.classes_)
+# plt.xlabel('Predicted')
+# plt.ylabel('True')
+# plt.title('Confusion Matrix')
+# plt.show()
